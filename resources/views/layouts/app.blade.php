cd5cd83f7386e771ece64f93107b3ce5140e1a82
MarkEslao <maxwellrenard@gmail.com>
2017-12-06 19:06:04 +0800

    Fixed the login and registration forms

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/services">Services</a></li>
-                    <li><a href="#">???</a></li>
+                    <li><a href="#" class="button disabled">???</a></li>
                     <li><a href="#login">Log In</a></li>
                     <li><a href="#register">Register</a></li>
                 </ul>
@@ -42,9 +42,49 @@
             <!-- Intro -->
             <article id="login">
                 <h2 class="major">Login</h2>
+                <form method="post" action="#">
+                        <div class="field half first">
+                            <label for="demo-name">Username</label>
+                            <input type="text" name="user" id="username" value="" placeholder="" />
+                        </div>
+                        <div class="field half">
+                            <label for="demo-name">Password</label>
+                            <input type="password" name="password" id="password" value="" placeholder="" />
+                        </div>
+                        <ul class="actions">
+                            <li><input type="submit" value="Log In"/></li>
+                            <li><a href="#">Forgot your password?</a></li>
+                        </ul>
+                </form>
             </article>
             <article id="register">
                 <h2 class="major">Register</h2>
+                <form method="post" action="#">
+                        <div class="field half first">
+                            <label for="demo-name">First Name</label>
+                            <input type="text" name="firstname" id="firstname" value="" placeholder="Enter your first name" />
+                        </div>
+                        <div class="field half">
+                            <label for="demo-email">Last Name</label>
+                            <input type="text" name="lastname" id="lastname " value="" placeholder="Enter your last name" />
+                        </div>
+                        <div class="field half first">
+                            <label for="demo-name">Username</label>
+                            <input type="text" name="user" id="username" value="" placeholder="Enter a username of your choice" />
+                        </div>
+                        <div class="field half">
+                            <label for="demo-name">Password</label>
+                            <input type="password" name="password" id="password" value="" placeholder="Enter a password of your choice" />
+                        </div>
+                        <div class="field">
+                            <label for="demo-name">Email Address</label>
+                            <input type="email" name="email" id="email" value="" placeholder="Enter your email address" />
+                        </div>
+                        <ul class="actions">
+                            <li><input type="submit" value="Register"/></li>
+                            <li><input type="reset" value="Clear" /></li>
+                        </ul>
+                </form>
             </article>
         </div>
 
